1f5f8cb9d24b48699ffe1d116cd845ab5899789d
Laurent Almeras <laurent.almeras@kobalt.fr>
2024-08-22 19:24:20 +0200

    mise en place de la limitation de débit

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Le projet démontre la mise en place d'une instance postmulti pour réaliser la remise
 des mails selon 2 files de priorité.
 
+Les mails avec un header `X-Priority: slow` sont remis avec une queue dont le débit
+est paramétré par `default_destination_rate_delay` (configuration postfix-slow).
+
 ## Démarrer le service
 
 ```shell
@@ -18,9 +21,19 @@ docker exec -ti postfix-postmulti /bin/bash
 ## Envoi d'un mail
 
 ```shell
+# Priorité normale
 msmtp -C msmtprc -a docker -t <<EOF
 To: lalmeras@gmail.com
 Subject: test
+
+test
+EOF
+
+# Priorité lente
+msmtp -C msmtprc -a docker -t <<EOF
+To: lalmeras@gmail.com
+Subject: test
+X-Priority: slow
 
 test
 EOF
--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
 
-if [ -d "/postfix-conf" ]; then
-  echo "Sync configuration from /postfix-conf (old files may be kept, recreate container to reset)"
-  rsync -a /postfix-conf/ /etc/postfix/
-  echo "Configuration content"
-  find /etc/postfix
+if ! grep -q "multi_instance_enable = yes" /etc/postfix/main.cf; then
+  echo "Init multi-instance"
+  /usr/sbin/postmulti -e init
+  /usr/sbin/postmulti -I postfix-slow -G mta -e create
+  /usr/sbin/postmulti -I postfix-relay -G mta -e create
 fi
 
-postconf maillog_file=/dev/stdout
+if [ -d "/postfix-conf" ]; then
+  echo "Sync configuration from /postfix-conf (old files may be kept, recreate container to reset)"
+  rsync -a /postfix-conf/ /etc/
+  echo "Configuration content"
+  find /etc/postfix*
+fi
 
-exec /usr/sbin/postmulti -i - -p start-fg
+touch /var/log/maillog /var/log/maillog-slow /var/log/maillog-relay
+
+/usr/sbin/postfix start
+/usr/bin/tail -f /var/log/maillog*
